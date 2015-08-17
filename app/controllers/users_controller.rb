531ccdf9496b2e5c1329ef76f80d93e480939636
patrick <phloftus@gmail.com>
2015-08-16 23:47:59 -0400

    remember users and sign in new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,10 +16,10 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       redirect_to @user
     else
       render 'new'
-      raise "error"
     end
   end
 
